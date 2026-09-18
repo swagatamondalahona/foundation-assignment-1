850d9a71ec9d1d77157bc4e3c3465053cfaa26c2
Swagata <swagatamondal682@gmail.com>
2026-09-18 12:24:26 +0600

    2 number commplet

--- question-1.js
+++ question-1.js
@@ -7,3 +7,19 @@ function describeValue(value) {
         return `${type} | falsy`;
     }
 }
+// Question No. 2
+function getDayType(day) {
+    const dayName = day.toLowerCase();
+    switch (dayName) {
+        case "friday":
+        case "saturday":
+            return "Weekend";
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+        default: return "Invalid Day";
+    }
+}
