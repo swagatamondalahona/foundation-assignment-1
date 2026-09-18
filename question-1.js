ef07bfc6fe5ae4890572b7d0ef493167494724ee
Swagata <swagatamondal682@gmail.com>
2026-09-18 12:35:08 +0600

    Add cricket chase verdict function

--- question-1.js
+++ question-1.js
@@ -48,3 +48,24 @@ function getCngFare(distance, isNight = false, waitingMinutes = 0) {
     }
     return fare;
 }
+// Question No. 5
+const getChaseVerdict = (target, scored, ballsLeft) => {
+    const runsNeeded = target - scored;
+    if (runsNeeded <= 0) {
+        return "Won";
+    }
+    if (ballsLeft <= 0) {
+        return "Lost";
+    }
+    const requiredRate = (runsNeeded / ballsLeft) * 6;
+    let verdict;
+    if (requiredRate <= 6) {
+        verdict = "Comfortable";
+    }
+    else if (requiredRate <= 12) {
+        verdict = "Tough";
+    } else {
+        verdict = "Almost Impossible";
+    }
+    return `Need ${runsNeeded} runs in ${ballsLeft} balls | ${verdict}`;
+};
