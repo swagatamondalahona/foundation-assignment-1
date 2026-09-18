3de46f024e5701ea0eec1aa65ac3da4857e85b70
Swagata <swagatamondal682@gmail.com>
2026-09-18 12:31:42 +0600

    Add CNG fare calculation

--- question-1.js
+++ question-1.js
@@ -36,3 +36,15 @@ function validateUsername(username) {
     }
     return "Available";
 }
+// Question No. 4
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+    let fare = 50;
+    if (distance > 2) {
+        fare = fare + (distance - 2) * 15;
+    }
+    fare = fare + waitingMinutes * 2;
+    if (isNight) {
+        fare = fare * 1.20;
+    }
+    return fare;
+}
